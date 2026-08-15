a96ea22027027d096e62e0851100b25fac09a210
Adam Winer <awiner@gmail.com>
2026-08-14 20:46:23 -0700

    Bump to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.scythebill.birdlist.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
