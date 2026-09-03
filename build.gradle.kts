99633f9ca99adf889821df260b1ee1ef9e1e4067
Adam Winer <awiner@gmail.com>
2026-09-02 18:46:47 -0700

    Bump KSP to 2.3.11, the most recent

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "2.3.10" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.3.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
-    id("com.google.devtools.ksp") version "2.3.4" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
 }
